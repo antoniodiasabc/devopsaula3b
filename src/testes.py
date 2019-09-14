ae4ee224b2fe67799e5d201358e630767b5ac2ca
antoniodiasabc <antoniodiasabc@gmail.com>
2019-09-13 21:39:12 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -1,15 +1,22 @@
-def inicializar():
-	tab = [ ]
-	for i in range(3):
-		linha = [ ]
-		for j in range(3):
-			linha.append(".")
-		tab.append(linha)
-	return tab
+Import jogovelha
+import sys
 
-def main( ):
-	jogo = inicializar( )
-	print (jogo)
+erroInicializar = False
+jogo = jogovelha.inicializar()
 
-if __name__ == "__main__":
-	main()
+if len(jogo) != 3:
+	erroInicializar = True
+else:
+	for linha in jogo:
+		if len(linha) != 3:
+			erroInicializar = True
+		else:
+			for elemento in linha:
+				if elemento != '.':
+					erroInicializar = True
+if erroInicializar:
+	sys.exit(1)
+else:
+	sys.exit(0)
+
+
